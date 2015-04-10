c9bbe55185ee4c2c5a94de3511386cf50986cf33
idurugkar <idurugkar@cs.umass.edu>
2015-04-10 16:17:13 -0400

    modify run-all

--- run-all.sh
+++ run-all.sh
@@ -1,5 +1,5 @@
-javaw -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.GatewayManager test-input.csv
-javaw -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.MotionSensor test-input.csv
-javaw -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.TemperatureSensor test-input.csv
-javaw -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.OutletDeviceController test-input.csv
-javaw -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.LightBulbDeviceController test-input.csv
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.GatewayManager test-input.csv &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.MotionSensor test-input.csv &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.TemperatureSensor test-input.csv &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.OutletDeviceController test-input.csv &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.LightBulbDeviceController test-input.csv &
