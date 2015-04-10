87fdc6bc1735bb54165a98027c8f402e8a60626a
idurugkar <idurugkar@cs.umass.edu>
2015-04-10 16:00:45 -0400

    run-all-custom-input.sh added

---
+++ run-all-custom-input.sh
@@ -0,0 +1,6 @@
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.GatewayManager &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.MotionSensor &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.TemperatureSensor &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.OutletDeviceController &
+java -classpath  SmarterHomes.jar com.assignment2.dos.smarterHomes.LightBulbDeviceController &
+echo hello
